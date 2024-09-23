9f25981ddbee34431732a125a355df49fa90928a
obikosato <obiko.sato@gmail.com>
2024-09-23 12:56:02 +0900

    2024-09-23T12:56:02

--- gas.js
+++ gas.js
@@ -1,28 +1,74 @@
-const getProps = (props) => {
-  const scriptProps = PropertiesService.getScriptProperties();
-  return scriptProps.getProperty(props);
-};
+// スクリプトプロパティから指定されたプロパティを取得する関数
+const getProps = (props) =>
+  PropertiesService.getScriptProperties().getProperty(props);
 
-// POSTリクエストを受ける
+// POSTリクエストを処理する関数
 const doPost = (e) => {
-  const slackToken = JSON.stringify(e)
-    .split(",")
-    .find((e) => e.match(/^\\"token/))
-    .split('"')[3]
-    .split("\\")[0];
+  // POSTデータを取得し、デコードしてJSONにパース
+  const data = e.postData.contents;
+  const payload = JSON.parse(decodeURIComponent(data.split("payload=")[1]));
 
+  // Slackトークンを取得し、検証用トークンと比較
+  const slackToken = payload.token;
   const expectedToken = getProps("SLACK_VERIFICATION_TOKEN");
 
+  // トークンが一致しない場合はUnauthorizedを返す
   if (slackToken !== expectedToken) {
     return ContentService.createTextOutput("Unauthorized").setMimeType(
       ContentService.MimeType.TEXT
     );
   }
-  postTopicToSlack();
+
+  // アクションIDを取得し、存在しない場合はエラーメッセージを返す
+  const actionId = payload.actions[0].action_id;
+  if (!actionId) {
+    return ContentService.createTextOutput("No action_id").setMimeType(
+      ContentService.MimeType.TEXT
+    );
+  }
+
+  // アクションIDに基づいて処理を分岐
+  switch (actionId) {
+    case "change_topic":
+      postTopicToSlack();
+      break;
+    case "choose_topic":
+      recordTopicUsage(payload.actions[0].value);
+      break;
+  }
+
+  // 処理が成功した場合はOKを返す
+  return ContentService.createTextOutput("OK").setMimeType(
+    ContentService.MimeType.TEXT
+  );
 };
 
-// main
+// Slackにお題を投稿する関数
 const postTopicToSlack = () => {
+  const selectedTopic = getCandidateTopic();
+  postToSlack(topicToPayloadBlocks(selectedTopic));
+};
+
+// お題の使用履歴を記録する関数
+const recordTopicUsage = (topic) => {
+  const data = getSpreadsheetData();
+  const today = new Date();
+  data.some((item, i) => item[0] === topic && (record(i + 1, today), true));
+};
+
+// お題の使用履歴をクリアする関数
+const clearTopicUsage = () => {
+  const data = getSpreadsheetData();
+  data.forEach((_, i) => record(i + 1, ""));
+};
+
+// 指定された行に日付を記録する関数
+const record = (rowIndex, date) => {
+  getTopicSpreadSheet().getRange(rowIndex, 2).setValue(date);
+};
+
+// 候補のお題を取得する関数
+const getCandidateTopic = () => {
   const data = getSpreadsheetData();
   const twoWeeksAgo = new Date();
   twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 14);
@@ -32,32 +78,28 @@ const postTopicToSlack = () => {
     return lastUsedDate < twoWeeksAgo || !item[1] ? [...acc, item[0]] : acc;
   }, []);
 
-  if (candidates.length === 0) return;
+  if (candidates.length === 0) {
+    clearTopicUsage();
+    return getCandidateTopic();
+  }
 
   const randomIndex = Math.floor(Math.random() * candidates.length);
-  const selectedTopic = candidates[randomIndex];
-  Logger.log(selectedTopic);
-
-  postToSlack(topicToPayloadBlocks(selectedTopic));
-  recordTopicUsage(selectedTopic);
+  return candidates[randomIndex];
 };
 
-const recordTopicUsage = (topic) => {
-  const data = getSpreadsheetData();
-  const today = new Date();
-  const record = (rowIndex, date) =>
-    getTopicSpreadSheet().getRange(rowIndex, 2).setValue(date);
-  data.some((item, i) => item[0] === topic && (record(i + 1, today), true));
-};
-
-const getTopicSpreadSheet = () =>
-  SpreadsheetApp.openById(getProps("SPREADSHEET_ID")).getSheetByName(
+// スプレッドシートを取得する関数
+const getTopicSpreadSheet = () => {
+  return SpreadsheetApp.openById(getProps("SPREADSHEET_ID")).getSheetByName(
     "お題管理シート"
   );
+};
 
-const getSpreadsheetData = () =>
-  getTopicSpreadSheet().getDataRange().getValues();
+// スプレッドシートのデータを取得する関数
+const getSpreadsheetData = () => {
+  return getTopicSpreadSheet().getDataRange().getValues();
+};
 
+// Slackにメッセージを投稿する関数
 const postToSlack = (blocks) => {
   const slackUrl = "https://slack.com/api/chat.postMessage";
   const options = {
@@ -71,12 +113,13 @@ const postToSlack = (blocks) => {
   Logger.log(res);
 };
 
+// お題をSlackのメッセージブロック形式に変換する関数
 const topicToPayloadBlocks = (topic) => [
   {
     type: "section",
     text: {
       type: "plain_text",
-      text: `今日のお題: ${topic}`,
+      text: `今日のお題: ${topic || "お題がありません!!"}`,
     },
   },
   {
@@ -89,6 +132,15 @@ const topicToPayloadBlocks = (topic) => [
           text: "別のお題！",
         },
         action_id: "change_topic",
+      },
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          text: "これにする！",
+        },
+        value: topic,
+        action_id: "choose_topic",
       },
     ],
   },
