5f5b4e0fdaed1f5fea1bdb6494ad1d9a6cb0ef3c
obikosato <obiko.sato@gmail.com>
2025-03-22 13:51:04 +0900

    ロジックを簡略化

--- gas.js
+++ gas.js
@@ -1,18 +1,13 @@
 // メイン関数
 const main = () => {
-  // お題取得
   const topics = fetchTopicsFromSpreadsheet();
-  const seed = seedForToday();
-  const index = generateIndex(topics.length, seed);
-  const selectedTopic = topics[index];
+  const selectedTopic = topics[getIndex(topics.length)];
   if (!selectedTopic) {
     Logger.log("お題が取得できませんでした");
     return;
   }
 
-  // Slackに投稿
-  const blocks = topicToPayloadBlocks(selectedTopic);
-  const res = postToSlack(blocks);
+  const res = postToSlack(selectedTopic);
   logResponse(res);
 };
 
@@ -25,47 +20,36 @@ const fetchTopicsFromSpreadsheet = () =>
     .map((row) => row[0])
     .filter((topic) => topic);
 
-// 今日の日付を元に乱数のシードを生成する関数
-const seedForToday = () =>
-  parseInt(Utilities.formatDate(new Date(), "JST", "yyyyMMdd"), 10);
-
-// 乱数を生成する関数
-const generateIndex = (size, seed) => {
-  let state = seed;
-  state ^= (state << 13) & 0xffffffff;
-  state ^= state >>> 17;
-  state ^= (state << 5) & 0xffffffff;
-
-  return Math.abs(state) % size;
+// 1日ごとにインクリメントされるインデックスを取得する関数
+const getIndex = (size) => {
+  const base = new Date(2025, 0, 1);
+  const now = new Date();
+  const elapsedDays = Math.floor((now - base) / (1000 * 60 * 60 * 24));
+  return elapsedDays % size;
 };
 
-// お題をSlackのメッセージブロック形式に変換する関数
-const topicToPayloadBlocks = (topic) => [
-  {
-    type: "section",
-    text: {
-      type: "plain_text",
-      text: `今日のお題: ${topic}`,
-    },
-  },
-];
-
 // Slackにメッセージを投稿する関数
-const postToSlack = (blocks) => {
+const postToSlack = (topic) => {
   const slackUrl = "https://slack.com/api/chat.postMessage";
   const options = {
     method: "post",
     contentType: "application/json; charset=utf-8",
     headers: { Authorization: "Bearer " + getProps("SLACK_BOT_TOKEN") },
-    payload: JSON.stringify({ channel: getProps("SLACK_CHANNEL_ID"), blocks }),
+    payload: JSON.stringify({
+      channel: getProps("SLACK_CHANNEL_ID"),
+      blocks: [
+        {
+          type: "section",
+          text: { type: "plain_text", text: `今日のお題: ${topic}` },
+        },
+      ],
+    }),
   };
-
   return UrlFetchApp.fetch(slackUrl, options);
 };
 
 // レスポンスをログに出力する関数
 const logResponse = (res) => {
-  console.log(res.getContentText());
   try {
     const jsonResponse = JSON.parse(res.getContentText());
     Logger.log(JSON.stringify(jsonResponse, null, 2));
