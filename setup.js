f19bbc4add07609f08b8be71b9ea7ee40be2c6ba
obikosato <obiko.sato@gmail.com>
2025-03-22 13:26:23 +0900

    Google Spreadsheetのシート名をプロパティに追加

--- setup.js
+++ setup.js
@@ -1,4 +1,5 @@
 const SPREADSHEET_ID = ""; // Google SpreadsheetのID
+const SPREADSHEET_SHEET_NAME = "お題管理シート"; // Google Spreadsheetのシート名
 const SLACK_BOT_TOKEN = ""; // Slack Botトークン
 const SLACK_VERIFICATION_TOKEN = ""; // Slackから取得したVerification Token
 const SLACK_CHANNEL_ID = ""; // SlackのチャンネルID
@@ -7,6 +8,7 @@ const SLACK_CHANNEL_ID = ""; // SlackのチャンネルID
 const setup = () => {
   const props = PropertiesService.getScriptProperties();
   props.setProperty("SPREADSHEET_ID", SPREADSHEET_ID);
+  props.setProperty("SPREADSHEET_SHEET_NAME", SPREADSHEET_SHEET_NAME);
   props.setProperty("SLACK_BOT_TOKEN", SLACK_BOT_TOKEN);
   props.setProperty("SLACK_VERIFICATION_TOKEN", SLACK_VERIFICATION_TOKEN);
   props.setProperty("SLACK_CHANNEL_ID", SLACK_CHANNEL_ID);
