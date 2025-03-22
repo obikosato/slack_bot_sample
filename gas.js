c8963f63425cc8f4dfb7bd4d4f0ac14fce83e847
obikosato <obiko.sato@gmail.com>
2025-03-22 13:27:03 +0900

    色々アップデート - Xorshift使ってみる - Logをパースしてみる

--- gas.js
+++ gas.js
@@ -1,44 +1,42 @@
-// スクリプトプロパティから指定されたプロパティを取得する関数
-const getProps = (props) =>
-  PropertiesService.getScriptProperties().getProperty(props);
+// メイン関数
+const main = () => {
+  // お題取得
+  const topics = fetchTopicsFromSpreadsheet();
+  const seed = seedForToday();
+  const index = generateIndex(topics.length, seed);
+  const selectedTopic = topics[index];
+  if (!selectedTopic) {
+    Logger.log("お題が取得できませんでした");
+    return;
+  }
 
-// Slackにお題を投稿する関数
-const postTopicToSlack = () => {
-  const selectedTopic = getCandidateTopic();
-  postToSlack(topicToPayloadBlocks(selectedTopic));
+  // Slackに投稿
+  const blocks = topicToPayloadBlocks(selectedTopic);
+  const res = postToSlack(blocks);
+  logResponse(res);
 };
 
-// 候補のお題を取得する関数
-const getCandidateTopic = () => {
-  const data = getSpreadsheetData();
-  const candidates = data.map((row) => row[0]).filter((topic) => topic);
+// スプレッドシートからお題のリストを取得する関数
+const fetchTopicsFromSpreadsheet = () =>
+  SpreadsheetApp.openById(getProps("SPREADSHEET_ID"))
+    .getSheetByName(getProps("SPREADSHEET_SHEET_NAME"))
+    .getDataRange()
+    .getValues()
+    .map((row) => row[0])
+    .filter((topic) => topic);
 
-  const randomIndex = Math.floor(Math.random() * candidates.length);
-  return candidates[randomIndex];
-};
+// 今日の日付を元に乱数のシードを生成する関数
+const seedForToday = () =>
+  parseInt(Utilities.formatDate(new Date(), "JST", "yyyyMMdd"), 10);
 
-// スプレッドシートを取得する関数
-const getTopicSpreadSheet = () =>
-  SpreadsheetApp.openById(getProps("SPREADSHEET_ID")).getSheetByName(
-    "お題管理シート"
-  );
+// 乱数を生成する関数
+const generateIndex = (size, seed) => {
+  let state = seed;
+  state ^= (state << 13) & 0xffffffff;
+  state ^= state >>> 17;
+  state ^= (state << 5) & 0xffffffff;
 
-// スプレッドシートのデータを取得する関数
-const getSpreadsheetData = () =>
-  getTopicSpreadSheet().getDataRange().getValues();
-
-// Slackにメッセージを投稿する関数
-const postToSlack = (blocks) => {
-  const slackUrl = "https://slack.com/api/chat.postMessage";
-  const options = {
-    method: "post",
-    contentType: "application/json",
-    headers: { Authorization: "Bearer " + getProps("SLACK_BOT_TOKEN") },
-    payload: JSON.stringify({ channel: getProps("SLACK_CHANNEL_ID"), blocks }),
-  };
-
-  const res = UrlFetchApp.fetch(slackUrl, options);
-  Logger.log(res);
+  return Math.abs(state) % size;
 };
 
 // お題をSlackのメッセージブロック形式に変換する関数
@@ -47,7 +45,36 @@ const topicToPayloadBlocks = (topic) => [
     type: "section",
     text: {
       type: "plain_text",
-      text: `今日のお題: ${topic || "お題がありません!!"}`,
+      text: `今日のお題: ${topic}`,
     },
   },
 ];
+
+// Slackにメッセージを投稿する関数
+const postToSlack = (blocks) => {
+  const slackUrl = "https://slack.com/api/chat.postMessage";
+  const options = {
+    method: "post",
+    contentType: "application/json; charset=utf-8",
+    headers: { Authorization: "Bearer " + getProps("SLACK_BOT_TOKEN") },
+    payload: JSON.stringify({ channel: getProps("SLACK_CHANNEL_ID"), blocks }),
+  };
+
+  return UrlFetchApp.fetch(slackUrl, options);
+};
+
+// レスポンスをログに出力する関数
+const logResponse = (res) => {
+  console.log(res.getContentText());
+  try {
+    const jsonResponse = JSON.parse(res.getContentText());
+    Logger.log(JSON.stringify(jsonResponse, null, 2));
+  } catch (e) {
+    Logger.log("レスポンスのパースに失敗しました: " + e.message);
+    Logger.log(res.getContentText());
+  }
+};
+
+// スクリプトプロパティから指定されたプロパティを取得する関数
+const getProps = (props) =>
+  PropertiesService.getScriptProperties().getProperty(props);
